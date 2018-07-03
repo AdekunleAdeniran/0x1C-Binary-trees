614c66476755439d0eef1a7660d1ca781d82de21
flourishcodes <kunle@adeniran.com>
2018-07-02 23:10:46 -0700

    C function to delete binary tree

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -4,6 +4,8 @@ void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return;
+
+	/*recursively search for all nodes left and right and delete them*/
 	binary_tree_delete(tree->left);
 	binary_tree_delete(tree->right);
 
