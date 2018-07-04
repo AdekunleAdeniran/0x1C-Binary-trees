fada67c27d74b8f7518e56b77f9d0fdaa5465f63
flourishcodes <kunle@adeniran.com>
2018-07-03 23:18:48 -0700

    C function to print level order in binary tree

---
+++ 101-binary_tree_levelorder.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+/**
+ *
+ *
+ *
+ */
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+
+	if (tree->parent == NULL)
+		func(tree->n);
+	if (tree->left != NULL)
+		func(tree->left->n);
+	if (tree->right != NULL)
+		func(tree->right->n);
+	if (tree->left != NULL)
+		binary_tree_levelorder(tree->left, func);
+	if (tree->right != NULL)
+		binary_tree_levelorder(tree->right,func);
+}
